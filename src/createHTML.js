393222d24cdde877bbae3c921a5e8aa30b51fe92
Stone Comstock <comstockstone@yahoo.com>
2022-02-27 21:37:09 -0800

    fixed formatting and finished createHTML.js file

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,6 +1,6 @@
 //function to create manager card
 const createManager = (manager) => {
-    return `
+  return `
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
@@ -15,11 +15,11 @@ const createManager = (manager) => {
         </div>
     </div>
     `;
-}
+};
 
 //function to create engineer card
 const createEngineer = (engineer) => {
-    return `
+  return `
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
@@ -33,12 +33,12 @@ const createEngineer = (engineer) => {
             </div>
         </div>
     </div>
-    `
-}
+    `;
+};
 
 //function to create intern card
 const createIntern = (intern) => {
-    return `
+  return `
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header">
@@ -52,5 +52,84 @@ const createIntern = (intern) => {
             </div>
         </div>
     </div>
-    `
+    `;
 };
+//function to create cards for each user submitted employee, then push to an array
+// push array to page
+const createHTML = (data) => {
+  // initialize empty array, will be filled with all employees from user submitted data
+  employeeArray = [];
+  //loop through all employees in data from inquirer, create a roll variable for each employee
+  for (let i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
+
+    // if role is manager, call createManager function
+    if (role === 'Manager') {
+      const managerCard = createManager(employee);
+      //push new card to employeeArray
+      employeeArray.push(managerCard);
+    }
+
+    // if role is engineer, call createEngineer function
+    if (role === 'Engineer') {
+      const engineerCard = createEngineer(employee);
+      //push new card to employeeArray
+      employeeArray.push(engineerCard);
+    }
+
+    // if role is intern, call createIntern function
+    if (role === 'Intern') {
+      const internCard = generateIntern(employee);
+      //push new card to employeeArray
+      employeeArray.push(internCard);
+    }
+  }
+
+  //turn array into a string with .join method. (needs to be a string since its all HTML)
+  const cards = employeeArray.join('');
+
+  //call createTeamPage function passing in string of generated cards, save to variable and return it.
+  const generateTeam = createTeamPage(cards);
+  return generateTeam;
+};
+
+const createTeamPage = (cards) => {
+  //return HTML page with links to stylesheet, bootstrap, font awesome, and jquery/bootstrap script tags.
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${cards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
+  `;
+};
+
+// export createHTML function
+module.exports = createHTML;
